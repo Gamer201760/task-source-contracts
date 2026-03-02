61190f458370e9bab3fa4f419714be4436c2ea83
Azamat <azamat201760@yandex.ru>
2026-03-02 11:17:54 +0300

    [feat] add sleep in api mock

--- repository/api/mock.py
+++ repository/api/mock.py
@@ -1,6 +1,9 @@
+import time
+
 from domain.job import Job
 
 
 class MockExternalSource:
     def get_tasks(self) -> list[Job]:
+        time.sleep(1)  # задержка, имитация хождения по сети
         return [Job(id=1, payload={'palka': 'copalka'})]
